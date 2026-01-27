5f06ca527e812ea08407b92fa769b29a38289be4
MauricioSanchezFernandez <mausanfer16@gmail.com>
2026-01-27 19:54:04 +0100

    playercontrolerº

---
+++ 2weeksgame_project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+
+    //variables de referencia-> conectado con el personaje
+
+    //investigar por que no se pone azul
+    private Rigidbody2D playerRb;
+    private Animator anim;
+    private float horizontalInput;
+
+
+    //variables de estadisticas del player
+    public float speed;
+    public float jumpForce;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = Getcomponent<Rigibody2D>();
+        anim = Getcomponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
