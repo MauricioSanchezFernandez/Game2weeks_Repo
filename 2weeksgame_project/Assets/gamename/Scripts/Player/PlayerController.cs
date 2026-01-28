85e55bad989b4b7b7e3e31210380b0e0fe9f0b58
MauricioSanchezFernandez <mausanfer16@gmail.com>
2026-01-28 10:34:01 +0100

    cositas

--- 2weeksgame_project/Assets/gamename/Scripts/Player/PlayerController.cs
+++ 2weeksgame_project/Assets/gamename/Scripts/Player/PlayerController.cs
@@ -3,46 +3,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    //variables de ref
-    private Rigidbody2D playerRb;
-    private Animator anim;
-    private float horizontalInput;
+    [Header("Movement and Jump Configuration")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
 
-    //variabless  estadistica
-    public float speed;
-    public float jumpForce;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerRb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-        Jump();
+        
     }
-
-    void Movement()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        playerRb.linearVelocityX= new Vector2(horizontalInput * speed, playerRb.linearVelocityX.y);
-    
-    }
-
-    void Jump()
-    {
-        playerRb.AddForce(Vector3.up * jumpForce);
-
-    }
-
 }
-
-
-
-
-
