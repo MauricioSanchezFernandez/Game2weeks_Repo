47cc69beb09f9e956450344f2d9c5285b7e8c134
MauricioSanchezFernandez <mausanfer16@gmail.com>
2026-01-29 10:32:48 +0100

    ayuda

--- 2weeksgame_project/Assets/gamename/Scripts/Player/PlayerController.cs
+++ 2weeksgame_project/Assets/gamename/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -12,14 +13,39 @@ public class PlayerController : MonoBehaviour
 
 
     //Variables estdisticas del player
+    [Header("movement Configuration")]
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
+    [SerializeField] bool isFacingRight; //define orientacion del personaje
     
+
+    [Header("GroundCheck Configuration")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] Transform groundCheck; //Posicion del detector del suelo
+    [SerializeField] float groundCheckRadius; //Define el radio del circulo detector de suelo
+    [SerializeField] LayerMask groundLayer; //Define la capa que puede tocar el detector de suelo
+
+    [Header("Dash Configuration")]
+     bool canDash = true;  //puede dashear?
+    bool isDashing = false; //esta dasheando?
+    [SerializeField] float powerDash;//potencia del dash
+    [SerializeField] float timeDashing; //tiempo de dash
+    [SerializeField] float cooldownDash; //cooldown del dash quien lo diria
+    [SerializeField] TrailRenderer tr; //efecto dash
+
+
+
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>(); //autoreferencias un componente propio
+        anim = GetComponent<Animator>();
+       
+    }
 
     void Start()
     {
-        playerRb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+               isFacingRight = true;
     }
 
     
@@ -27,20 +53,47 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         Jump();
+        Dash();
+
+        if (isDashing)
+        { 
+            return;
+        }
+
+        //Logica de deteccion del suelo
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        //logica flip del personaje
+              
+        if (horizontalInput > 0 && !isFacingRight) Flip();
+        if (horizontalInput < 0 && isFacingRight) Flip();
     }
+
+        private void FixedUpdate()
+    {
+
+        if (isDashing)
+        {
+            return;
+        }
+
+        Movement();
+    }
+
 
     //INPUTS
     void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        //playerRb.angularVelocity = new Vector2(horizontalInput * speed, playerRb.linearVelocity.y);
+        playerRb.linearVelocity = new Vector2(horizontalInput * speed, playerRb.linearVelocity.y);
 
                 
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
         { playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse); }
         
     
@@ -56,9 +109,9 @@ public class PlayerController : MonoBehaviour
 
     void Dash()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Mouse1)) //pulsar1
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Mouse1) && canDash) //pulsar1
         {
-            //config dash
+            StartCoroutine(Dashc());
         }
     }
 
@@ -69,6 +122,35 @@ public class PlayerController : MonoBehaviour
             //config interact
         }
     }
+
+    void Flip()
+    {
+
+        Vector3 currentScale = transform.localScale; //almacen temporal de la escala del objeto
+        currentScale.x *= -1; //invertir el valor x
+        transform.localScale = currentScale; //le devolvemos la escala al objeto con el valor en x inverso
+       isFacingRight = !isFacingRight; //decirle al bool que cambie al valor contrario
+
+    }
+
+    //corrutina dash-corrutinas
+    IEnumerator Dashc()
+    { 
+     canDash = false;
+        isDashing = true;
+        float originalGravity = playerRb.gravityScale;
+        playerRb.gravityScale = 0;
+        playerRb.linearVelocity = new Vector2(transform.localScale.x * powerDash, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(timeDashing);
+        tr.emitting = false;
+        playerRb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(cooldownDash);
+        canDash = true;
+
+    }
+
 
 }
 
