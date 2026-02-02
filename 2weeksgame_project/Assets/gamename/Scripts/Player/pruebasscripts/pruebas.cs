58d96778cf86836d49524c9d8fa6dbcfe17bb3ea
MauricioSanchezFernandez <mausanfer16@gmail.com>
2026-02-02 11:23:43 +0100

    Reapply "hggfhftrh"
    This reverts commit 54cd56bbca17b83f76ed6bf30b5e49584dd11f99.

--- 2weeksgame_project/Assets/gamename/Scripts/Player/pruebasscripts/pruebas.cs
+++ 2weeksgame_project/Assets/gamename/Scripts/Player/pruebasscripts/pruebas.cs
@@ -28,6 +28,8 @@ public class pruebas : MonoBehaviour
     float gravedadInicial;
     bool puedeHacerDash = true;
     bool sePuedeMover = true;
+    [SerializeField] TrailRenderer trailRenderer;
+
 
     [Header("Animacion")]
     Animator animator;
@@ -106,10 +108,11 @@ public class pruebas : MonoBehaviour
         puedeHacerDash = false;
         playerRb.linearVelocity = new Vector2(velocidadDash * transform.localScale.x, 0);
         yield return new WaitForSeconds(tiempoDash);
-
+        trailRenderer.emitting = true;
         sePuedeMover = true;
         puedeHacerDash = true;
         playerRb.gravityScale = gravedadInicial;
+        trailRenderer.emitting = false;
 
     }
 
